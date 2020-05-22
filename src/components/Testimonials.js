cf71da4781d9b58ea6545c9ba46f2da7205fa63d
Sam Odum <sam.odum1@gmail.com>
2020-05-22 13:34:11 +0100

    Fix testimonial block height
    Because:
    - The previous implementation skewed the height
    
    This fixes the height issue of the testimonial component.

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,55 +1,56 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'react-animated-slider';
 // import 'react-animated-slider/build/vertical.css';
 import '../styles/modules/Testimonial.scss';
 import Clients from './Clients';
 
-const Testimony = ({ testimonyData }) => {
+const testimonyData = [
+  {
+    id: 1,
+    comment:
+      'CR-Mart is an excellent system and has helped us reach out to millions of customer base across the world. We have managed to hugely ramp up our revenue. Thanks to CR-Mart team!',
+    image: 'https://via.placeholder.com/90x90.png',
+    name: 'Ifeanyi Ihemeje',
+    title: 'Ceo, Focus Tianshi Global',
+  },
+  {
+    id: 2,
+    comment:
+      'This is the best system that we have come across so far! Keep up the good work! You really have helped us in a great manner! Cheers!',
+    image: 'https://via.placeholder.com/90x90.png',
+    name: 'Cyril Chukwu',
+    title: 'Director, Platini Concept',
+  },
+  {
+    id: 1,
+    comment:
+      'We highly recommend CR-Mart system as its the most flexible, versatile system that is on offering. It has all required features to enable any business to move to a fast running track and get running at a rapid pace!',
+    image: 'https://via.placeholder.com/90x90.png',
+    name: 'Patrick Edet',
+    title: 'General Manager, Dojopa Limited',
+  },
+];
+
+const Testimony = ({ data }) => {
   return (
-    <>
-      <p>{testimonyData.comment}</p>
+    <div className="holder">
+      <p>{data.comment}</p>
       <div className="thumb">
         <img
           className="mylazy"
-          src={testimonyData.image}
-          testimonyData-src="chooseOneImageLater"
-          alt={testimonyData.name}
+          src={data.image}
+          data-src="chooseOneImageLater"
+          alt={data.name}
         />
       </div>
-      <strong className="name">{testimonyData.name}</strong>
-      <strong className="designation">{testimonyData.title}</strong>
-    </>
+      <strong className="name">{data.name}</strong>
+      <strong className="designation">{data.title}</strong>
+    </div>
   );
 };
 
 const Testimonials = () => {
-  const [testimonial] = useState([
-    {
-      id: 1,
-      comment:
-        'CR-Mart is an excellent system and has helped us reach out to millions of customer base across the world. We have managed to hugely ramp up our revenue. Thanks to CR-Mart team!',
-      image: 'https://via.placeholder.com/90x90.png',
-      name: 'Ifeanyi Ihemeje',
-      title: 'Ceo, Focus Tianshi Global',
-    },
-    {
-      id: 2,
-      comment:
-        'This is the best system that we have come across so far! Keep up the good work! You really have helped us in a great manner! Cheers!',
-      image: 'https://via.placeholder.com/90x90.png',
-      name: 'Cyril Chukwu',
-      title: 'Director, Platini Concept',
-    },
-    {
-      id: 1,
-      comment:
-        'We highly recommend CR-Mart system as its the most flexible, versatile system that is on offering. It has all required features to enable any business to move to a fast running track and get running at a rapid pace!',
-      image: 'https://via.placeholder.com/90x90.png',
-      name: 'Patrick Edet',
-      title: 'General Manager, Dojopa Limited',
-    },
-  ]);
   return (
     <div className="container">
       <div className="row">
@@ -61,41 +62,30 @@ const Testimonials = () => {
                 className="product-flexslider hidden-buttons home-testimonials"
               >
                 <div className="slider-items slider-width-col4 ">
-                  <div className="holder">
-                    <Slider
-                      direction="vertical"
-                      autoplay={8000}
-                      classNames={{
-                        slider: 'slider-v',
-                        previousButton: 'prevButton',
-                        nextButton: 'nexButton',
-                        buttonDisabled: 'disabled',
-                        track: 'track',
-                        slide: 'slide-v',
-                        hidden: 'hidden',
-                        previous: 'previous',
-                        current: 'current',
-                        next: 'next',
-                        animateIn: 'animateIn',
-                        animateOut: 'animateOut',
-                      }}
-                    >
-                      {testimonial.map((item) => (
-                        <div
-                          key={item.id}
-                          style={{
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                          }}
-                        >
-                          <div>
-                            <Testimony testimonyData={item} />
-                          </div>
-                        </div>
-                      ))}
-                    </Slider>
-                  </div>
+                  <Slider
+                    direction="vertical"
+                    autoplay={8000}
+                    classNames={{
+                      slider: 'slider-v',
+                      previousButton: 'prevButton',
+                      nextButton: 'nexButton',
+                      buttonDisabled: 'disabled',
+                      track: 'track',
+                      slide: 'slide-v',
+                      hidden: 'hidden',
+                      previous: 'previous',
+                      current: 'current',
+                      next: 'next',
+                      animateIn: 'animateIn',
+                      animateOut: 'animateOut',
+                    }}
+                  >
+                    {testimonyData.map((testimonyDataItem) => (
+                      <div key={testimonyDataItem.id}>
+                        <Testimony data={testimonyDataItem} />
+                      </div>
+                    ))}
+                  </Slider>
                 </div>
               </div>
             </div>
@@ -108,7 +98,7 @@ const Testimonials = () => {
 };
 
 Testimony.propTypes = {
-  testimonyData: PropTypes.shape({
+  data: PropTypes.shape({
     comment: PropTypes.string,
     image: PropTypes.string,
     name: PropTypes.string,
